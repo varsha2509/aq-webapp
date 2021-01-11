975d1e868ed6c6bed9d10c2bfed684dd8293c02a
Varsha Gopalakrishnan <varsha.gopalakrishnan@gmail.com>
2021-01-10 16:37:09 -0800

    added polygon boundary

--- app.py
+++ app.py
@@ -37,8 +37,7 @@ def load_oakl_data():
 	file_id_oakl = original_url_oakl.split('/')[-2]
 	dwn_url_oakl = 'https://drive.google.com/uc?export=download&id=' + file_id_oakl
 	url_oakl = requests.get(dwn_url_oakl).content
-	path_oakl = StringIO(url_oakl) 
-	print(path_oakl)
+	path_oakl = StringIO(url_oakl.read().decode('utf-8')) 
 	oakl_geo = gpd.read_file(path_oakl, driver = 'GeoJSON',encoding="utf-8")
 	return oakl_geo
 
