ae2977881d0f1e1d4485737ab297757331852e1f
Varsha Gopalakrishnan <varsha.gopalakrishnan@gmail.com>
2021-01-10 17:12:39 -0800

    added polygon boundary

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def convert_address(address):
 
 
 def display_map(point, df, oakl_geo):
-	m = folium.Map(point, tiles='OpenStreetMap', zoom_start=9)
+	m = folium.Map(point, tiles='OpenStreetMap', zoom_start=11)
 
 	# Add polygon boundary to folium map
 	_, oakl_json = load_oakl_data()
