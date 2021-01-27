38c9df0eba3633ca43ccda893b3165889a5aed36
Varsha Gopalakrishnan <varsha.gopalakrishnan@gmail.com>
2021-01-27 01:02:25 -0800

    updated webpage

--- web-application.py
+++ web-application.py
@@ -79,6 +79,13 @@ def display_map(point, df, oakl_geojson):
 	
 	return st.markdown(m._repr_html_(), unsafe_allow_html=True)
 
+## Adding a background to streamlit page
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+
+
 def main():
 	#Load csv data
 	df_data = load_data()
@@ -89,11 +96,14 @@ def main():
 	oakl_geojson = json.loads(oakl_json)
 
 	#For the page display, create headers and subheader, and get an input address from the user
+	local_css("style.css")
 	st.header("Predicting Air Quality in Oakland, California")
 	st.text("")
-	st.subheader("This website reports annual average concentrations of Black Carbon and Nitrogen Dioxide in Oakland.")
+	st.markdown('<p class="big-font">This web-app reports annual average concentrations of Black Carbon and Nitrogen Dioxide in Oakland. Concentrations are based on a machine learning model built to make predictions at any location in Oakland using publicly available data on major sources of emissions in Oakland and neighbouring cities, number of traffic intersections, proximity to highways, and local meteorological data.</p>', unsafe_allow_html=True)
 	st.text("")
-	address = st.text_input("Enter an address or point of interest below.", "900 Fallon St, Oakland, CA 94607")
+	st.markdown('<p class="big-font"> <b> Enter an address below and click on the marker to know the air quality at your place of interest. </b> </p>', unsafe_allow_html=True)
+
+	address = st.text_input("  ", "900 Fallon St, Oakland, CA 94607")
 
 	# Use the convert_address function to convert address to coordinates
 	coordinates = convert_address(address)
@@ -102,8 +112,15 @@ def main():
 	st.text("")
 	display_map(coordinates, df_data, oakl_geojson)
 
+
 	st.text("")
-	st.text("Created by - Varsha - https://www.linkedin.com/in/varshagopalakrishnan/")
+	mediumlink = """ <a href ="https://varsha-gopalakrishnan.medium.com/hyperlocal-air-quality-prediction-using-machine-learning-ed3a661b9a71">@Medium</a> """
+	st.markdown('<p> Check out this blog to learn more about how the machine learning models were developed. </p>', unsafe_allow_html = True)
+	st.markdown(mediumlink, unsafe_allow_html=True)
+	st.text("")
+	linkedinlink = """ <a href="https://www.linkedin.com/in/varshagopalakrishnan/">@LinkedIn</a> """
+	st.markdown('<p class = "small-font">  Created by - Varsha Gopalakrishnan </p>', unsafe_allow_html=True)
+	st.markdown(linkedinlink, unsafe_allow_html=True)
 
   
 if __name__ == "__main__":
